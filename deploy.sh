f952ab3e8ba2da1aa5f0d6ba8baf7cf44f276e1a
Jo Vandeginste <jo.vandeginste@gmail.com>
2020-01-04 11:35:18 +0100

    Reformat, add git stuff
    Signed-off-by: Jo Vandeginste <jo.vandeginste@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 source /etc/os-release
 
-cat << EOF
+cat <<EOF
 This script will update this system to a CoderDojo laptop as used in Rotselaar.
 If you are running this script by accident, you should exit now (type ctrl+c).
 
@@ -19,26 +19,32 @@ Press ctrl+c to abort now.
 EOF
 read
 
-if [[ "$USER" != "root" ]]
-then
-  cat << EOF
+if [[ "$USER" != "root" ]]; then
+	cat <<EOF
 This script requires superuser access. You should rerun it as:
 $ sudo $0 ${@@Q}
 EOF
-exit 1
+	exit 1
 fi
 
 set -x
 case "$NAME" in
-  Fedora)
-    yum -y install puppet
-    ;;
-  Ubuntu)
-    apt update
-    apt -y install puppet
-    ;;
-  *)
-    echo "Unknown/unsupported operating system. Bailing out." >&2
-    exit 1
-    ;;
+Fedora)
+	yum -y install puppet git
+	;;
+Ubuntu)
+	apt update
+	apt -y install puppet git
+	;;
+*)
+	echo "Unknown/unsupported operating system. Bailing out." >&2
+	exit 1
+	;;
 esac
+
+if [[ ! -d /var/lib/coderdojo-deploy ]]; then
+	git clone https://github.com/CoderDojoRotselaar/os-config /var/lib/coderdojo-deploy
+fi
+
+cd /var/lib/coderdojo-deploy
+git pull
