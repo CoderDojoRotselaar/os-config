57d3d53708881a79790380e7a6b7e71d756c1fa3
Jo Vandeginste <jo.vandeginste@gmail.com>
2020-01-05 14:17:36 +0100

    Add command support
    Signed-off-by: Jo Vandeginste <jo.vandeginste@gmail.com>

--- puppet-apply.sh
+++ puppet-apply.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 set -eu
 
-if ! ping -q -W 1 -i 1 -c 1 github.com; then
-	echo "No internet or github.com unreachable. Bailing out now."
-	exit 1
-fi
+function checkNetwork() {
+	if ! ping -q -W 1 -i 1 -c 1 github.com; then
+		echo "No internet or github.com unreachable. Bailing out now."
+		exit 1
+	fi
+}
 
+function updatePuppet() {
+	set -x
+	git pull
+	librarian-puppet update --verbose
+}
+
+cmd=${1:-update}
+shift
 REPOSITORY_ROOT=/var/lib/coderdojo-deploy
 
-set -x
-
 cd "${REPOSITORY_ROOT}"
-git pull
-librarian-puppet update --verbose
 
+case "${cmd}" in
+update)
+	checkNetwork
+	updatePuppet
+	;;
+esac
+
+set -x
 puppet apply "${REPOSITORY_ROOT}/manifests/site.pp"
