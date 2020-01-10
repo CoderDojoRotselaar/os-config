9d493456c6c29263757be66158c3f0a7784b4d22
Jo Vandeginste <jo.vandeginste@gmail.com>
2020-01-10 23:12:12 +0100

    Shallow clone is faster
    Signed-off-by: Jo Vandeginste <jo.vandeginste@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -24,47 +24,47 @@ EOF
 read
 
 if [[ "$USER" != "root" ]]; then
-	cat <<EOF
+  cat <<EOF
 This script requires superuser access. You should rerun it as:
 $ sudo $0 ${@@Q}
 EOF
-	exit 1
+  exit 1
 fi
 
 case "$NAME" in
 Fedora)
-	INSTALL_PRE_COMMAND="yum -y clean all"
-	INSTALL_COMMAND=yum
-	INSTALL_PRE_PARAMS="-y install"
-	GEM_INSTALL_PARAMS=""
-	;;
+  INSTALL_PRE_COMMAND="yum -y clean all"
+  INSTALL_COMMAND=yum
+  INSTALL_PRE_PARAMS="-y install"
+  GEM_INSTALL_PARAMS=""
+  ;;
 Ubuntu)
-	INSTALL_PRE_COMMAND="apt update"
-	INSTALL_COMMAND=apt
-	INSTALL_PRE_PARAMS="-y install"
-	GEM_INSTALL_PARAMS="--no-ri --no-rdoc"
-	;;
+  INSTALL_PRE_COMMAND="apt update"
+  INSTALL_COMMAND=apt
+  INSTALL_PRE_PARAMS="-y install"
+  GEM_INSTALL_PARAMS="--no-ri --no-rdoc"
+  ;;
 *)
-	echo "Unknown/unsupported operating system. Bailing out." >&2
-	exit 1
-	;;
+  echo "Unknown/unsupported operating system. Bailing out." >&2
+  exit 1
+  ;;
 esac
 
 set -x
 
 if ! command -v puppet >/dev/null; then
-	echo "Puppet not yet installed - installing now..."
-	$INSTALL_PRE_COMMAND
-	$INSTALL_COMMAND $INSTALL_PRE_PARAMS puppet git-core
+  echo "Puppet not yet installed - installing now..."
+  $INSTALL_PRE_COMMAND
+  $INSTALL_COMMAND $INSTALL_PRE_PARAMS puppet git-core
 fi
 
 if ! command -v librarian-puppet >/dev/null; then
-	echo "Librarian-puppet not yet installed - installing now..."
-	gem install $GEM_INSTALL_PARAMS librarian-puppet
+  echo "Librarian-puppet not yet installed - installing now..."
+  gem install $GEM_INSTALL_PARAMS librarian-puppet
 fi
 
 if [[ ! -d "${REPOSITORY_ROOT}" ]]; then
-	git clone https://github.com/CoderDojoRotselaar/os-config "${REPOSITORY_ROOT}"
+  git clone --depth 1 https://github.com/CoderDojoRotselaar/os-config "${REPOSITORY_ROOT}"
 fi
 
 ${REPOSITORY_ROOT}/puppet-apply.sh
